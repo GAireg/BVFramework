5bb10eef23f258330cc67f366b17d41a3e4b2ea1
Randolph Maaßen <gaireg@gaireg.de>
2015-09-30 22:40:00 +0200

    added gpl header

--- WidgetCaptionMatLabel.h
+++ WidgetCaptionMatLabel.h
@@ -1,3 +1,25 @@
+/***********************************************************
+ *
+ * WidgetCaptionMatLabel.h - A MatLabel with a Caption
+ *
+ * Copyright 2015 Randolph Maaßen
+ *
+ **********************************************************/
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef WIDGETCAPTIONMATLABEL_H
 #define WIDGETCAPTIONMATLABEL_H
 
